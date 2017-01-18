713a5584c297961f8a51927892c8e86531919417
jthompson18 <john.thompson@4cinsights.com>
2017-01-17 20:49:20 -0600

    update dependencies and add eslint.js file

---
+++ .eslintrc.js
@@ -0,0 +1,24 @@
+module.exports = {
+    "extends": "airbnb",
+    "plugins": [
+        "react",
+        "jsx-a11y",
+        "import"
+    ],
+    "rules": {
+	    'func-names': 0,
+	    'prefer-arrow-callback': 0,
+	    'new-cap': 0,
+	    'camelcase': 0,
+	    'no-constant-condition': 0,
+	    'no-param-reassign': 0,
+	    'space-before-function-paren': 0,
+	    'import/no-unresolved': 0,
+	    'import/no-extraneous-dependencies': 0,
+	    'comma-dangle': 0,
+	    'no-underscore-dangle': 0
+	},
+	"env": {
+	    "es6": true
+	  }
+};
